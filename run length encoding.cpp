250247da3270fde0abfba726c6f0cdfef3426dca
Nilesh9910 <nileshkumar998@gmail.com>
2022-01-01 19:58:47 +0530

    day 1

---
+++ run length encoding.cpp
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+string encode(string src);    
+ 
+int main() {
+	
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		string str;
+		cin>>str;
+		
+		cout<<encode(str)<<endl;
+	}
+	return 0;
+}// } Driver Code Ends
+
+
+/*You are required to complete this function */
+
+string encode(string s)
+{    
+    int n = s.length();
+    for (int i = 0; i < n; i++) {
+        int count = 1;
+        while (i < n - 1 && s[i] == s[i + 1]) {
+            count++;
+            i++;
+        }
+        cout << s[i] << count;
+    }
+}     
+ 
